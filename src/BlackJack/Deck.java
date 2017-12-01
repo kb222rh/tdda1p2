176d156c3c81719f325ed452bf0550a8489fe373
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-01 18:16:24 +0100

    RED, deck constructor

---
+++ src/BlackJack/Deck.java
@@ -0,0 +1,15 @@
+package BlackJack;
+
+public class Deck {
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Card drawCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/TestSuite/utDeck.java
@@ -0,0 +1,20 @@
+package TestSuite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import BlackJack.Card;
+import BlackJack.Deck;
+public class utDeck {
+
+	@Test
+	public void Deck_constructor_has52Cards() {
+		Deck sut=new Deck();
+		assertTrue(sut.size()==52);
+		for (int i=0; i<sut.size(); i++)
+        assertTrue(sut.drawCard() instanceof Card);
+	}
+
+}
+
