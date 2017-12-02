b0d0a01e59feea3010b069c7ae7439a68a621b61
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-02 19:54:33 +0100

    RED, player hand

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -1,9 +1,16 @@
 package BlackJack;
 
+import java.util.ArrayList;
+
 public class Player {
 
 	public Card getCard(Deck d) {
 		return d.drawCard();
+	}
+
+	public ArrayList<Card> showHand() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/TestSuite/utPlayer.java
+++ src/TestSuite/utPlayer.java
@@ -2,6 +2,8 @@ package TestSuite;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -25,7 +27,21 @@ public class utPlayer {
 		Mockito.when(cardStub.getFace()).thenReturn("Ace of Spades");
 		Card actual= p.getCard(deckStub);
 		assertEquals(deckStub.drawCard(), actual);
-
 	}
-
+	@Test
+	public void Player_showHand_ReturnsCardsDrawnAsArrayList(){
+		Deck deckStub=Mockito.mock(Deck.class);
+		Card cardStub1=Mockito.mock(Card.class);
+		Card cardStub2=Mockito.mock(Card.class);
+		Mockito.when(deckStub.drawCard()).thenReturn(cardStub1);
+		Mockito.when(cardStub1.getFace()).thenReturn("Ace of Spades");
+		p.getCard(deckStub);
+		Mockito.when(deckStub.drawCard()).thenReturn(cardStub2);
+		Mockito.when(cardStub2.getFace()).thenReturn("Ace of Clubs");
+		p.getCard(deckStub);
+		ArrayList<Card> actual= p.showHand();
+		
+		assertEquals(cardStub1, actual.get(0));
+		assertEquals(cardStub2, actual.get(1));
+	}
 }
