5ddcc5da01e8476ce2947da3d62302a4e63ce1a9
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-02 18:12:00 +0100

    REFACTOR

--- src/TestSuite/utDeck.java
+++ src/TestSuite/utDeck.java
@@ -1,29 +1,36 @@
 package TestSuite;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import BlackJack.Card;
 import BlackJack.Deck;
 public class utDeck {
+	private Deck a;
+	private Deck b;
 	
+	@Before
+	public void setup(){
+		 a=new Deck();
+		 b=new Deck();
+	}
 	@Test
 	public void Deck_constructor_has52Cards() {
-		Deck sut=new Deck();
-		assertTrue(sut.size()==52);
-		for (int i=0; i<sut.size(); i++)
-        assertTrue(sut.drawCard() instanceof Card);
+
+		assertTrue(a.size()==52);
+		for (int i=0; i<a.size(); i++)
+        assertTrue(a.drawCard() instanceof Card);
 	}
 	@Test
 	public void Deck_equals_ReturnsTrueForNewDecks(){
-		Deck a=new Deck();
-		Deck b=new Deck();
+	
 		assertTrue(a.equals(b));
 	}
 	@Test
 	public void Deck_equals_ReturnsFalseAfterShuffle(){
-		Deck a=new Deck();
-		Deck b=new Deck();
+
 		b.shuffle();
 		assertFalse(a.equals(b));
 	}
