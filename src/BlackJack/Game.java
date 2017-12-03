df2a4aa6a4e1ab2b07c923a58805796b234d2972
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-03 21:40:47 +0100

    refactor, added a graphic display after stand for ease of use

--- src/BlackJack/Game.java
+++ src/BlackJack/Game.java
@@ -34,6 +34,7 @@ public class Game {
 				while(dealer.calcScore()<18)
 					dealer.getCard(deck);
 				play=false;
+				displayStatus(player, dealer);
 				isWinner(player,dealer);
 			} else if (input.hitSignal()&&!over(player)){
 				player.getCard(deck);
