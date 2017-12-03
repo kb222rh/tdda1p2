ff176802ece16ca8f3d33b493517ead776375c13
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-03 19:09:44 +0100

    green

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -20,8 +20,16 @@ public class Player {
 
 	public int calcScore() {
 		int sum=0;
+		int aceCount=0;
 		for (Card c : hand){
+			if (c.getValue()==11){
+				aceCount++;
+			}
 			sum+=c.getValue();
+			while (sum>21&&aceCount>0){
+				sum-=10;
+				aceCount--;
+			}
 		}
 		return sum;
 	}
