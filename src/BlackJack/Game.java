3f6f9693788ee05c9943596bbb7b596b66411361
Kirill Bacha <kb222rh@student.lnu.se>
2017-12-03 20:31:58 +0100

    green

--- src/BlackJack/Game.java
+++ src/BlackJack/Game.java
@@ -53,7 +53,7 @@ public class Game {
 		return p.calcScore()>21;
 	}
 	public void isWinner(Player p, Player d){
-		if (d.calcScore()>=p.calcScore()&&!over(d))
+		if (d.calcScore()>=p.calcScore()&&!over(d)||over(p))
 		this.out.println("Dealer wins!");
 		else 
 		this.out.println("Player wins!");
